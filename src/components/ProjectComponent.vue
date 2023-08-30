fd4e440ec55dee6bad833a8e67511f70d6f50d3b
Thomas Testaud <thomas.testaud@hotmail.fr>
2023-08-30 16:25:34 +0200

    ...

--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -38,18 +38,110 @@
           <button v-if="drawingToolsData.currentTool === toolsMetaData.scissors" class="selected"><img src="@/assets/scissors.png" alt="dots"></button>
           <button v-else @click="selectTool(toolsMetaData.scissors)"><img src="@/assets/scissors.png" alt="spray"></button>
 
-          <button><img src="@/assets/dots.png" alt="dots"></button>
-          <button><img src="@/assets/dots.png" alt="dots"></button>
-          
           <button v-if="drawingToolsData.currentTool === toolsMetaData.eraser" class="selected"><img src="@/assets/eraser.png" alt="eraser"></button>
           <button v-else @click="selectTool(toolsMetaData.eraser)"><img src="@/assets/eraser.png" alt="spray"></button>
           
           <button v-if="drawingToolsData.currentTool === toolsMetaData.four" class="selected"><img src="@/assets/bucket.png" alt="bucket"></button>
-          <button v-else @click="selectTool(toolsMetaData.four)"><img src="@/assets/bucket.png" alt="spray"></button>
-
-          <button @click="blackCanvas"><img src="@/assets/empty-canvas.png" alt="empty-canvas"></button>
-          <button @click="emptyCanvas"><img src="@/assets/clearcanvas.png" alt="clearcanvas"></button>
+          <button v-else @click="selectTool(toolsMetaData.four)"><img src="@/assets/bucket.png" alt="spray"></button>   
         </div>
+        
+        <div class="edit-section">
+          <div>
+            <div class="sub-text">Layer:</div> 
+            <div>
+              <button class="hover-tag-parent" @click="createNewLayer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+                <div class="hover-tag-children">
+                  Create new layer
+                </div>
+              </button>
+              <button class="hover-tag-parent selected" v-if="renamingLayer" @click="renameLayerFalse">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
+                <div class="hover-tag-children">
+                  Rename layers
+                </div>
+              </button>
+              <button class="hover-tag-parent" v-else @click="renameLayerTrue">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
+                <div class="hover-tag-children">
+                  Rename layers
+                </div>
+              </button>
+              <button class="hover-tag-parent" @click="deleteSelectedLayer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+                <div class="hover-tag-children">
+                  Delete layer
+                </div>
+              </button>
+            </div>
+          </div>
+          <div>
+            <div class="sub-text">Frame:</div> 
+            <div>
+              <button class="hover-tag-parent" @click="createNewFrame">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+                <div class="hover-tag-children">
+                  Create new frame
+                </div>
+              </button>
+              <button class="hover-tag-parent">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" /></svg>
+                <div class="hover-tag-children">
+                  Copy frame
+                </div>
+              </button>
+              <button class="hover-tag-parent">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" /></svg>
+                <div class="hover-tag-children">
+                  Paste frame
+                </div>
+              </button>
+              <button class="hover-tag-parent" @click="duplicateSelectedFrame">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /></svg>
+                <div class="hover-tag-children">
+                  Duplicate selected frame
+                </div>
+              </button>
+              <br>
+              <button class="hover-tag-parent" @click="deleteSelectedFrame">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+                <div class="hover-tag-children">
+                  Delete selected frame
+                </div>
+              </button>
+            </div>
+          </div>
+          <div>
+            <div class="sub-text">Image:</div> 
+            <div>
+              <button class="hover-tag-parent" @click="blackCanvas">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+                <div class="hover-tag-children">
+                  Create empty image
+                </div>
+              </button>
+              <button class="hover-tag-parent">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" /></svg>
+                <div class="hover-tag-children">
+                  Copy image
+                </div>
+              </button>
+              <button class="hover-tag-parent">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" /></svg>
+                <div class="hover-tag-children">
+                  Paste image
+                </div>
+              </button>
+              <button class="hover-tag-parent" @click="emptyCanvas">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+                <div class="hover-tag-children">
+                  Delete image
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+
       </div>
 
       <div class="canvas-container">
@@ -63,6 +155,12 @@
           <button @click="play"><img src="@/assets/play.png" alt="play"></button>
           <button @click="nextFrame"><img src="@/assets/rightskip.png" alt="rightskip"></button>
         </div>
+
+        <span id="frame-parameters">
+          <label for="frame-rate">Frame rate: </label>
+          <input type="number" id="frame-rate" v-model="frameRate" @keyup="updateFrameRate" @click="updateFrameRate">
+          /s
+        </span>
       </div>
       
       <div class="tool-section tool-section-right">
@@ -112,7 +210,7 @@
       </div>
     </div>
     
-    
+    <!--
     <div id="frame-parameters">
       <span class="params">
         <button @click="createNewLayer">Create new layer</button>
@@ -130,7 +228,7 @@
         <input type="number" id="frame-rate" v-model="frameRate" @keyup="updateFrameRate" @click="updateFrameRate">
         /s
       </span>
-    </div>
+    </div>-->
 
     <div id="lower-section">
       <div id="timeline" @click="readCurrentFrame">
@@ -807,7 +905,8 @@ export default {
     border-top: 1px dashed var(--border-color);
   }
 
-  #tool-parameters label {
+  #tool-parameters label,
+  .sub-text {
     font-size: 12px;
     opacity: 0.8;
     margin-left: 5px;
@@ -895,10 +994,17 @@ export default {
     height: 100%;
   }
 
+  .canvas-container {
+    position: relative;
+  }
+
   #frame-parameters {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    position: absolute;
+    bottom: 15px;
+    right: -6px;
   }
 
   #frame-parameters input {
@@ -1069,13 +1175,58 @@ export default {
     width: 15px;
   }
 
+  .edit-section {
+    background-color: var(--element2-background-color);
+    border-top: 1px solid var(--border-color);
+    border-radius: 3px;
+  }
+
+  .edit-section button {
+    height: 30px;
+    width: 30px;
+    padding: 0px;
+    margin: 2px;
+  }
+
+  .edit-section svg {
+    height: 100%;
+  }
+
+  .edit-section > div + div {
+    border-top: 1px dashed var(--border-color);
+  }
+
+  .hover-tag-parent {
+    position: relative;
+  }
+
+  .hover-tag-children {
+    display: none;
+    opacity: 0;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    transform: translate(80%, -100%);
+    transition: opacity 0.2s;
+    background-color: var(--element-background-color);
+    color: white;
+    border: 2px solid var(--border-color);
+    border-radius: 5px;
+    z-index: 2;
+    white-space: nowrap;
+    padding: 3px;
+  }
+
+  .hover-tag-parent:hover .hover-tag-children {
+    display: block;
+    opacity: 1;
+  }
 
   @media (max-width: 1220px) {
     #upper-section {
       flex-wrap: wrap;
       justify-content: space-evenly;
     }
-    
   
     #upper-section > div:nth-child(1) {
       order: 1; 
