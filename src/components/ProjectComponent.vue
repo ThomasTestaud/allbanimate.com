bee1ef2ab013b6b5af0252d6b92362870358a99f
Thomas Testaud <thomas.testaud@hotmail.fr>
2023-08-30 18:12:09 +0200

    ...

--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -84,13 +84,13 @@
                   Create new frame
                 </div>
               </button>
-              <button class="hover-tag-parent">
+              <button @click="copyFrame" class="hover-tag-parent">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" /></svg>
                 <div class="hover-tag-children">
                   Copy frame
                 </div>
               </button>
-              <button class="hover-tag-parent">
+              <button @click="pasteFrame" class="hover-tag-parent">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" /></svg>
                 <div class="hover-tag-children">
                   Paste frame
@@ -120,13 +120,13 @@
                   Create empty image
                 </div>
               </button>
-              <button class="hover-tag-parent">
+              <button class="hover-tag-parent" @click="copyImage">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" /></svg>
                 <div class="hover-tag-children">
                   Copy image
                 </div>
               </button>
-              <button class="hover-tag-parent">
+              <button class="hover-tag-parent" @click="pasteImage">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">  <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" /></svg>
                 <div class="hover-tag-children">
                   Paste image
@@ -322,6 +322,8 @@ export default {
       onionValue: this.projectData.onionValue,
       onionLayerState: this.projectData.onionLayerState,
       
+      copiedFrame: null,
+      copiedImage: null,
       interval: null,
       canvas: null,
       canvasData: null,
@@ -379,6 +381,30 @@ export default {
     this.readCurrentFrame();
   },
   methods: {
+
+    pasteImage() {
+      if(this.copiedImage !== null) {
+        this.calkLayers[this.selectedCalk].code[this.displayedFrame] = this.calkLayers[this.copiedImage[1]].code[this.copiedImage[0]]
+        this.readCurrentFrame();
+      }
+    },
+
+    copyImage() {
+      this.copiedImage = [this.displayedFrame, this.selectedCalk];
+    },
+
+    pasteFrame() {
+      if(this.copiedFrame !== null) {
+        this.calkLayers.forEach((element) => {
+          element.code[this.displayedFrame] = element.code[this.copiedFrame];
+        });
+        this.readCurrentFrame();
+      }
+    },
+
+    copyFrame() {
+      this.copiedFrame = this.displayedFrame;
+    },
 
     renameLayerTrue() {
       this.renamingLayer = true;
@@ -1204,9 +1230,9 @@ export default {
     display: none;
     opacity: 0;
     position: absolute;
-    top: 0px;
-    right: 0px;
-    transform: translate(80%, -100%);
+    top: -1px;
+    right: 20px;
+    transform: translate(100%, -100%);
     transition: opacity 0.2s;
     background-color: var(--element-background-color);
     color: white;
@@ -1217,9 +1243,14 @@ export default {
     padding: 3px;
   }
 
+  
   .hover-tag-parent:hover .hover-tag-children {
-    display: block;
+    display: flex;
     opacity: 1;
+  }
+  .hover-tag-parent:hover .hover-tag-children:hover {
+    display: none;
+    opacity: 0;
   }
 
   @media (max-width: 1220px) {
